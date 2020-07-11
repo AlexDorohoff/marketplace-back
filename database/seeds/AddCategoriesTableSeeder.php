79a389335df5d273c8e9d0cfc69a909110d9419f
AlexDor <32705886+AlexDorohoff@users.noreply.github.com>
2020-07-11 17:24:34 +0300

    15 главных категорий и 30 подкатегорий

--- database/seeds/AddCategoriesTableSeeder.php
+++ database/seeds/AddCategoriesTableSeeder.php
@@ -11,105 +11,33 @@ class AddCategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('category')->insert([
-            'id' => 7,
-            'name' => 'категория 2',
-            'parent_id' => null,
-        ]);
 
-        DB::table('category')->insert([
-            'id' => 8,
-            'name' => '1 подкатегория категории 2',
-            'parent_id' => 7,
-        ]);
+        $i = 0;
+        while ($i < 45) {
+            DB::table('category')->insert([
+                'id' => $i,
+                'name' => 'категория 2',
+                'parent_id' => null,
+            ]);
+            $i = $this->insertChild($i);
+        }
+    }
 
-        DB::table('category')->insert([
-            'id' => 9,
-            'name' => '2 подкатегория категории 2',
-            'parent_id' => 7,
-        ]);
+    public function insertChild($parent_id)
+    {
+        for ($j = 1; $j <= 5; $j++) {
+            $id = $parent_id + $j;
+            DB::table('category')->insert([
+                'id' => $id,
+                'name' => $j . ' подкатегория категории' . $parent_id,
+                'parent_id' => $parent_id,
+            ]);
+        }
+        return ++$id;
+    }
 
-        DB::table('category')->insert([
-            'id' => 10,
-            'name' => '3 подкатегория категории 2',
-            'parent_id' => 7,
-        ]);
-
-        DB::table('category')->insert([
-            'id' => 11,
-            'name' => '4 подкатегория категории 2',
-            'parent_id' => 7,
-        ]);
-
-        DB::table('category')->insert([
-            'id' => 12,
-            'name' => 'категория 3',
-            'parent_id' => null,
-        ]);
-
-        DB::table('category')->insert([
-            'id' => 13,
-            'name' => '1 подкатегория категории 3',
-            'parent_id' => 12,
-        ]);
-
-        DB::table('category')->insert([
-            'id' => 14,
-            'name' => '2 подкатегория категории 3',
-            'parent_id' => 12,
-        ]);
-
-        DB::table('category')->insert([
-            'id' => 15,
-            'name' => '3 подкатегория категории 3',
-            'parent_id' => 12,
-        ]);
-
-        DB::table('category')->insert([
-            'id' => 16,
-            'name' => 'категория 4',
-            'parent_id' => null,
-        ]);
-
-        DB::table('category')->insert([
-            'id' => 17,
-            'name' => '1 подкатегория категории 4',
-            'parent_id' => 15,
-        ]);
-
-        DB::table('category')->insert([
-            'id' => 18,
-            'name' => '2 подкатегория категории 4',
-            'parent_id' => 15,
-        ]);
-
-        DB::table('category')->insert([
-            'id' => 19,
-            'name' => '3 подкатегория категории 4',
-            'parent_id' => 16,
-        ]);
-        DB::table('category')->insert([
-            'id' => 20,
-            'name' => '4 подкатегория категории 4',
-            'parent_id' => 12,
-        ]);
-
-        DB::table('category')->insert([
-            'id' => 21,
-            'name' => 'категория 5',
-            'parent_id' => null,
-        ]);
-
-        DB::table('category')->insert([
-            'id' => 22,
-            'name' => '1 подкатегория категории 5',
-            'parent_id' => 21,
-        ]);
-
-        DB::table('category')->insert([
-            'id' => 23,
-            'name' => '2 подкатегория категории 5',
-            'parent_id' => 21,
-        ]);
+    public function down()
+    {
+        Schema::dropIfExists('category');
     }
 }
